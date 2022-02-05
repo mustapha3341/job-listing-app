42f799329ef82174bbd8eb78c2eade7bec530d7c
mustapha3341 <mustapha3341@gmail.com>
2022-02-05 10:08:20 +0100

    project setup

---
+++ src/components/Job/Job.jsx
@@ -0,0 +1,9 @@
+const Job = () => {
+    return (
+        <article>
+            <p>Job Post</p>
+        </article>
+    );
+};
+
+export default Job;
---
+++ src/components/JobList/JobList.jsx
@@ -0,0 +1,9 @@
+const JobList = () => {
+    return (
+        <div>
+            <p>Job Listing</p>
+        </div>
+    );
+};
+
+export default JobList;
